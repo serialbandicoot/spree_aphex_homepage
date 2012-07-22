ecd18164c6196bdf03917588d90ab8ae0a85153b
Sam <samtreweek@gmail.com>
2012-07-22 19:55:40 +0100

    removed cupcake from design

--- lib/generators/spree_aphex_homepage/install/install_generator.rb
+++ lib/generators/spree_aphex_homepage/install/install_generator.rb
@@ -1,20 +1,20 @@
-module SpreeLilyandmaxHomepage
+module SpreeAphexHomepage
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_lilyandmax_homepage\n"
+        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_aphex_homepage\n"
         #append_file "app/assets/javascripts/shared/all.js", "//= require shared/spree_lilyandmax_homepage\n"
         append_file "app/assets/javascripts/store/all.js", "//= require store/jquery.cycle.all\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_lilyandmax_homepage\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_aphex_homepage\n", :before => /\*\//, :verbose => true
         #inject_into_file "app/assets/stylesheets/shared/all.css", " *= require shared/spree_lilyandmax_homepage\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_lilyandmax_homepage'
+        run 'bundle exec rake railties:install:migrations FROM=spree_aphex_homepage'
       end
 
       def run_migrations
